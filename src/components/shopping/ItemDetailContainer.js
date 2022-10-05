03c147ae3cf602a1d2373f0e915afaadf5983896
juan-ps <juanps.rt@gmail.com>
2022-10-05 18:34:48 -0300

    segunda preentrega

--- src/components/shopping/ItemDetailContainer.js
+++ src/components/shopping/ItemDetailContainer.js
@@ -4,7 +4,7 @@ import { products } from "./data/products"
 
 const ItemDetailContainer = () => {
   const { id: itemId } = useParams();
-  const [item, setItem] = useState({})
+  const [item, setItem] = useState({});
 
   useEffect(() => {
     getItemDetails().then(res => {
@@ -20,18 +20,23 @@ const ItemDetailContainer = () => {
     })
   }
 
+  const shortInfo = item.shortInfo.map((info) => <li>{info}</li>);
+
   return (
     <>
       <div>
         <div className="text-4xl my-5">
-          <p>{item.name}</p>
+          <p><strong>{item.name}</strong></p>
         </div>
-        <div className="container grid grid-cols-2 mx-auto">
-          <figure className="max-w-sm">
-            <img src={item.image.imgB} />
+        <div className="container min-h-screen grid grid-cols-3 grid-rows-2 mx-auto">
+          <figure className="max-w-sm row-span-2">
+            <img src={item.image.imgA} />
           </figure>
-          <div className="card max-w-md">
+          <div className="card max-w-md text-center">
             <p>{item.description}</p>
+          </div>
+          <div className="text-center">
+             {shortInfo}
           </div>
         </div>
       </div>
